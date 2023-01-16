86148b65feb0e0ef0bab6b26c8f55db99e184e44
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-16 12:40:43 +0900

    feat: Layout / create layout components - main, player

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import classes from './App.module.css';
+import CalendarNav from './layout/main/CalenderNav';
+import TrackGroup from './layout/main/TrackGroup';
+import TrackList from './layout/main/TrackList';
+import Player from './layout/player/Player';
+
 function App() {
-    return <></>;
+    return (
+        <div className={classes.container}>
+            <nav>
+                <CalendarNav />
+            </nav>
+            <main>
+                <TrackList />
+                <TrackGroup />
+                <Player />
+            </main>
+        </div>
+    );
 }
 
 export default App;
