cee3460d1442ce59499165556c71e125e9e6bb91
yeonjeong2 <digh2165@naver.com>
2023-01-18 15:12:42 +0900

    ex

---
+++ src/test/java/MusicProject/MusicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package MusicProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
