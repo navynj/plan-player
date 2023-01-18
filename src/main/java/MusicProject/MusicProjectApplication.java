1f46856777958b6e78fd630f4c880e9f11a071d2
yeonjeong2 <digh2165@naver.com>
2023-01-18 15:12:42 +0900

    ex

---
+++ src/main/java/MusicProject/MusicProjectApplication.java
@@ -0,0 +1,13 @@
+package MusicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicProjectApplication.class, args);
+	}
+
+}
