b489e50106dbd685a40f878b29602ffca7f2984a
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2023-01-27 07:04:48 +0900

    feat: Auth / Create screen for login/register #5

---
+++ src/main/frontend/src/layout/Auth.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef, useState } from 'react';
+import classes from './Auth.module.css';
+
+function Auth() {
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
+    useEffect(() => {
+        usernameRef.current!.focus();
+    }, [usernameRef, isLoginMode]);
+
+    const changeModeHandler = () => {
+        setIsLoginMode((prevState) => !prevState);
+    };
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredUsername = usernameRef.current!.value;
+        const enteredPassword = passwordRef.current!.value;
+        console.log(enteredUsername, enteredPassword);
+    };
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.info}>
+                <img
+                    src={require('../assets/logo.png')}
+                    alt="Plan player logo. Play button like letter P"
+                />
+                <h1>Plan Player</h1>
+            </div>
+            <div className={classes.form}>
+                <div className={classes.errors}></div>
+                <form onSubmit={submitHandler}>
+                    <div className={classes.inputs}>
+                        <div className={classes.field}>
+                            <input
+                                ref={usernameRef}
+                                id="username"
+                                type="text"
+                                name="username"
+                                required
+                            />
+                            <label htmlFor="username">Username</label>
+                        </div>
+                        <div className={classes.field}>
+                            <input
+                                ref={passwordRef}
+                                id="password"
+                                type="password"
+                                name="password"
+                                required
+                            />
+                            <label htmlFor="password">Password</label>
+                        </div>
+                    </div>
+                    <button type="submit">
+                        {isLoginMode ? 'Login' : 'Register'}
+                    </button>
+                    <div className={classes.links}>
+                        {isLoginMode && (
+                            <>
+                                <button type="button">Guset Login</button>
+                                <span>|</span>
+                            </>
+                        )}
+                        <button type="button" onClick={changeModeHandler}>
+                            {isLoginMode ? 'Register' : 'Back to login'}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
